cf4e922b2d55ae5030e209a10882005be4790c19
Adrian Vatchinsky <akv36@cornell.edu>
2017-04-05 09:10:23 -0400

    using hololens input manager for all events

--- ARlane/Assets/Scripts/CardManager.cs
+++ ARlane/Assets/Scripts/CardManager.cs
@@ -5,12 +5,13 @@ using UnityEngine.Events;
 using HoloToolkit.Unity.InputModule;
 using System;
 
+
 /// <summary>
-/// The Interactible class flags a Game Object as being "Interactible".
-/// Determines what happens when an Interactible is being gazed at.
+/// Responsible for handling the card events
 /// </summary>
-public class Interactible : MonoBehaviour, IInputClickHandler
+public class CardManager : MonoBehaviour, IInputClickHandler, IFocusable
 {
+
     public Material frontCard;
     public Material frontCardActive;
     public int rotationSpeed = 10;
@@ -19,40 +20,46 @@ public class Interactible : MonoBehaviour, IInputClickHandler
     private GameObject back;
     private bool canRotate;
     private int rotationCount = 0;
-    
-    void Start()
-    {
+
+    // Use this for initialization
+    void Start () {
         front = gameObject.transform.Find("Front").gameObject;
     }
-
-    void Update()
-    {
-        if(canRotate)
+	
+	// Update is called once per frame
+	void Update () {
+        if (canRotate)
         {
             RotateCard();
         }
     }
 
-    void GazeEntered()
-    {
-        front.GetComponent<Renderer>().material = frontCardActive;
-    }
-    void GazeExited()
-    {
-        front.GetComponent<Renderer>().material = frontCard;
-    }
-    
+
+
+
     public void OnInputClicked(InputEventData eventData)
     {
         canRotate = true;
     }
 
+    public void OnFocusEnter()
+    {
+        front.GetComponent<Renderer>().material = frontCardActive;
+    }
+
+    public void OnFocusExit()
+    {
+        front.GetComponent<Renderer>().material = frontCard;
+    }
+
+
+
     void RotateCard()
     {
         transform.Rotate(new Vector3(0, 1, 0), 10, Space.Self);
-  
+
         rotationCount++;
-        if(  180 / rotationSpeed == rotationCount )
+        if (180 / rotationSpeed == rotationCount)
         {
             rotationCount = 0;
             canRotate = false;
