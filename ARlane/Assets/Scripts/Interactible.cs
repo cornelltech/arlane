cbc09d3614a08aad13ca1e5d940a546e284f8660
Adrian Vatchinsky <akv36@cornell.edu>
2017-04-05 01:45:10 -0400

    added rotation logic on click event handler for card

--- ARlane/Assets/Scripts/Interactible.cs
+++ ARlane/Assets/Scripts/Interactible.cs
@@ -1,38 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using HoloToolkit.Unity.InputModule;
+using System;
 
 /// <summary>
 /// The Interactible class flags a Game Object as being "Interactible".
 /// Determines what happens when an Interactible is being gazed at.
 /// </summary>
-public class Interactible : MonoBehaviour
+public class Interactible : MonoBehaviour, IInputClickHandler
 {
     public Material frontCard;
     public Material frontCardActive;
+    public int rotationSpeed = 10;
 
     private GameObject front;
     private GameObject back;
+    private bool canRotate;
+    private int rotationCount = 0;
     
     void Start()
     {
         front = gameObject.transform.Find("Front").gameObject;
     }
 
+    void Update()
+    {
+        if(canRotate)
+        {
+            RotateCard();
+        }
+    }
+
     void GazeEntered()
     {
-        print("GazeEntered");
         front.GetComponent<Renderer>().material = frontCardActive;
     }
     void GazeExited()
     {
-        print("GazeExited");
         front.GetComponent<Renderer>().material = frontCard;
     }
-
-    void OnSelect()
+    
+    public void OnInputClicked(InputEventData eventData)
     {
-        print("OnSelect");
-        //transform.Rotate(Vector3.right);
+        canRotate = true;
+    }
+
+    void RotateCard()
+    {
+        transform.Rotate(new Vector3(0, 1, 0), 10, Space.Self);
+  
+        rotationCount++;
+        if(  180 / rotationSpeed == rotationCount )
+        {
+            rotationCount = 0;
+            canRotate = false;
+        }
+
     }
 }
