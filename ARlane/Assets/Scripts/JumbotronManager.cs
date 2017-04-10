5035f90cfd9d4952d1dc5494a99ec5b4821d0a0e
Adrian Vatchinsky <akv36@cornell.edu>
2017-04-10 10:26:00 -0400

    added jumbotron w/ video

---
+++ ARlane/Assets/Scripts/JumbotronManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumbotronManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        playVideo();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void playVideo()
+    {
+        ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+    }
+}
